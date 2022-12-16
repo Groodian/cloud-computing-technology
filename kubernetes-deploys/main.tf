9bdbe800dcdc83e1e7873abef158e3b541bf414b
Tom Marek <tom.marek@stud.h-da.de>
2022-12-16 17:12:04 +0100

    fixed gitlab ci

--- kubernetes-deploys/main.tf
+++ kubernetes-deploys/main.tf
@@ -1,5 +1,9 @@
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host = "https://34.89.238.18:6443"
+
+    client_certificate     = file("~/.kube/client-cert.pem")
+    client_key             = file("~/.kube/client-key.pem")
+    cluster_ca_certificate = file("~/.kube/cluster-ca-cert.pem")
   }
 }
